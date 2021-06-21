2805e8511c6fe17146e4f09316165afa5666a930
username <1151555861@qq.com>
2021-06-21 14:55:59 +0800

    first commit

---
+++ chaper2.2-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace chaper2._2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           PrintNames(GetArrayList());
+            Console.ReadKey();
+        }
+         
+        static ArrayList GetArrayList()
+        {
+            ArrayList names = new ArrayList();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+        static void PrintNames(ArrayList names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
